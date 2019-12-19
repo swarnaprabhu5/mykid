fa17506ec7735f8b928b7a4eb4c63c8383670291
Navaneeth Malingan <navneetnivu07@gmail.com>
2019-12-19 23:00:23 +0530

    router data

--- src/components/common/NavButton.js
+++ src/components/common/NavButton.js
@@ -1,23 +1,28 @@
-import React from "react";
-import PropTypes from "prop-types";
-import classNames from "classnames";
-import { NavLink as RouteNavLink } from "react-router-dom";
-import {  NavLink, Button, Col } from "shards-react";
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { NavLink as RouteNavLink } from 'react-router-dom';
+import { NavLink, Button, Col } from 'shards-react';
 
-const NavButton = ({ item, className }) => {
-  const classes = classNames(
-    className,
-  );
+const NavButton = ({ item, data, className }) => {
+  const classes = classNames(className);
   return (
     <Col xs="12" sm="8" className={classes}>
-      <NavLink tag={RouteNavLink} to={item.to}>
+      <NavLink
+        tag={RouteNavLink}
+        to={{
+          pathname: item.to,
+          state: data
+        }}
+      >
         <Button outline theme="accent" size="sm">
-            <i className="material-icons">account_box</i> {item.title && <span>{item.title}</span>}
+          <i className="material-icons">account_box</i>{' '}
+          {item.title && <span>{item.title}</span>}
         </Button>
       </NavLink>
     </Col>
-  )
-}
+  );
+};
 
 NavButton.propTypes = {
   /**
